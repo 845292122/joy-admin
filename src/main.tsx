7ff09f9a155b5b63096fab7b5f6bf8ca686300ea
edison <845292122@qq.com>
2025-01-04 15:50:45 +0800

    去除unocss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import 'virtual:uno.css'
 import Router from './router'
 import AuthRouter from './router/helper/authRouter'
 
