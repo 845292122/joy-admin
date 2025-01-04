50fde995a983ed3bc6e966538654bdcc2082a68e
edison <845292122@qq.com>
2025-01-04 16:51:00 +0800

    add cssprovider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router'
 import AuthRouter from './router/helper/authRouter'
+import { CssVarsProvider, extendTheme } from '@mui/joy'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthRouter>
-        <Router />
-      </AuthRouter>
-    </BrowserRouter>
+    <CssVarsProvider theme={extendTheme({ cssVarPrefix: '' })}>
+      <BrowserRouter>
+        <AuthRouter>
+          <Router />
+        </AuthRouter>
+      </BrowserRouter>
+    </CssVarsProvider>
   </StrictMode>
 )
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,14 @@
+import Button from '@mui/joy/Button'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <div>
+        <Button variant="solid">Login</Button>
+      </div>
+      <div>
+        <Button>Login</Button>
+      </div>
+    </div>
+  )
 }
