03875b134fb265fd8863240d9d025e54fe9fb50a
edison <845292122@qq.com>
2025-01-04 17:04:30 +0800

    add baseline

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router'
 import AuthRouter from './router/helper/authRouter'
-import { CssVarsProvider, extendTheme } from '@mui/joy'
+import { CssBaseline, CssVarsProvider, extendTheme, GlobalStyles } from '@mui/joy'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CssVarsProvider theme={extendTheme({ cssVarPrefix: '' })}>
+      <CssBaseline />
+      <GlobalStyles styles={{}} />
       <BrowserRouter>
         <AuthRouter>
           <Router />
