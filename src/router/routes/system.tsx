4e7c240a0d62be63c349ddc7556a79b62efa83ba
edison <845292122@qq.com>
2025-01-09 22:57:30 +0800

    新增system路由

---
+++ src/router/routes/system.tsx
@@ -0,0 +1,45 @@
+import Layout from '~/layout'
+import { RouteType } from '..'
+import Account from '~/pages/system/Account'
+import Perm from '~/pages/system/Perm'
+import OperLog from '~/pages/system/OperLog'
+
+const systemRoutes: Array<RouteType> = [
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/system/account',
+        element: <Account />,
+        meta: {
+          title: '账户管理',
+          key: 'system:account',
+          requireAuth: true,
+          permission: 'system:account'
+        }
+      },
+      {
+        path: '/system/perm',
+        element: <Perm />,
+        meta: {
+          title: '权限管理',
+          key: 'system:perm',
+          requireAuth: true,
+          permission: 'system:perm'
+        }
+      },
+      {
+        path: '/system/operlog',
+        element: <OperLog />,
+        meta: {
+          title: '操作日志',
+          key: 'system:operlog',
+          requireAuth: true,
+          permission: 'system:operlog'
+        }
+      }
+    ]
+  }
+]
+
+export default systemRoutes
