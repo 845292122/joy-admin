4fa300e4ba62ec2cfdb70e9f11b2284a3a1ec391
edison <845292122@qq.com>
2025-01-15 21:48:08 +0800

    feat: routeMetaHooks

--- src/router/helper/authRouter.tsx
+++ src/router/helper/authRouter.tsx
@@ -3,7 +3,7 @@ import { routes, RouteType } from '..'
 import { useAtomValue } from 'jotai'
 import { permAtom, tokenAtom } from '~/store/auth.store'
 
-function findRoute(pathname: string, routes: Array<RouteType> = []): RouteType {
+export function findRoute(pathname: string, routes: Array<RouteType> = []): RouteType {
   let result: RouteType = {}
   for (const route of routes) {
     if (route.path === pathname) return route
